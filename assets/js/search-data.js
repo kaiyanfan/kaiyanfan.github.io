de9808da231d03163a885faae0bcde0108bc526f
kaiyanfan <nbfanks@gmail.com>
2026-03-29 22:34:57 +0000

    Deploying to gh-pages from @ kaiyanfan/kaiyanfan.github.io@dd067cc065e1f4232a9b48101c58b69f4c1c8247 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "post-gfw-technical-review-10-trojan-and-statistical-fingerprinting",
+        },{id: "post-gfw-technical-review-11-advanced-tls-evasion",
+        
+          title: "GFW Technical Review 11 – Advanced TLS Evasion",
+        
+        description: "The Modern Circumvention Stack with NaïveProxy, XTLS, and REALITY",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/gfw11/";
+          
+        },
+      },{id: "post-gfw-technical-review-10-trojan-and-statistical-fingerprinting",
         
           title: "GFW Technical Review 10 – Trojan and Statistical Fingerprinting",
         
